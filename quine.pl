b40c0971223f9ec76ba7ce17afe644ff2529012e
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-04-08 03:34:54 +0700

    Add basic checking of X and Y

--- quine.pl
+++ quine.pl
@@ -1,4 +1,5 @@
 % :- set_prolog_flag(verbose, silent).
+:- use_module(library(clpfd)).
 
 % Base case is when the length is enough
 number_binarylist(-1, _, []).
@@ -8,6 +9,12 @@ number_binarylist(Length, Number, BinaryList) :-
     number_binarylist(NextLength, Number, NextBinaryList),
     CurrentValue is (Number>>Length) mod 2,
     append([CurrentValue], NextBinaryList, BinaryList).
+
+% Check the number of difference of List
+diff_count([], [], 0).
+diff_count([X | List1], [Y | List2], Result) :-
+    diff_count(List1, List2, NextResult),
+    ((X \= Y) -> (Result is NextResult + 1); (Result is NextResult)).
 
 quine(N, Minterms, Output) :-
     % Get TwoPower
@@ -19,4 +26,15 @@ quine(N, Minterms, Output) :-
     % Asserts all the elements of minterms is between it
     maplist(call(between, 0, TwoPower), Numbers),
     maplist(call(number_binarylist, N - 1), Numbers, BinaryList),
-    Output = BinaryList.
+    % Output = BinaryList,
+    length(BinaryList, MintermsLength),
+    MintermsLengthMinusOne is MintermsLength - 1,
+    [X, Y] ins 0..MintermsLengthMinusOne,
+    X #< Y,
+    % X is 0,
+    % Y is 1,
+    nth0(X, BinaryList, ElementX),
+    nth0(Y, BinaryList, ElementY),
+
+    diff_count(ElementX, ElementY, 1),
+    Output = [X, Y].
