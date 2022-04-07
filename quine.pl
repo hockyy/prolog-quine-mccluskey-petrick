ee783d09066467a197137893f3a14e605267cc73
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-04-08 02:23:16 +0700

    Initial commit

---
+++ quine.pl
@@ -0,0 +1,6 @@
+% :- set_prolog_flag(verbose, silent).
+
+go(N, Minterms, Output) :- TwoPower is 2 ** N - 1, Output = TwoPower,
+    split_string(Minterms, ",", ",", SubStrings),
+    maplist(number_string, Numbers, SubStrings),
+    maplist(call(between, 0, TwoPower), Numbers).
---
+++ quine.py
@@ -0,0 +1,21 @@
+from pyswip import Prolog
+import re
+
+RESULT_VAR = "Result"
+
+def quine_wrapper(n, minterms):
+    prolog = Prolog()
+    prolog.consult("quine")
+    prolog.query("silent")
+    query = f"go({n}, '{minterms}', {RESULT_VAR})"
+    print(query)
+    results = prolog.query(query)
+    for result in results:
+        print(result[RESULT_VAR])
+        break
+
+if __name__ == "__main__":
+    n = int(input("Enter number of variables: "))
+    minterms = input("Enter minterms (separated by comma ','): ")
+    minterms = re.sub("\s+", "", minterms)
+    quine_wrapper(n, minterms)
